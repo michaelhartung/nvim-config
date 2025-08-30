e90e378f9c7907662c5af4612b77094c82af1fcf
Michael Hartung <github@hartung.studio>
2025-08-30 23:49:13 +0200

    start treesitter with lsp

--- init.lua
+++ init.lua
@@ -90,6 +90,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     if client:supports_method('text/Document/completion') then
       vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = false })
       vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end)
+      vim.treesitter.start()
     end
   end
 })
